d916bc0fdeed166349b2879951e2f685c30907d7
Pellizzon <38332600+Pellizzon@users.noreply.github.com>
2020-09-02 13:22:41 -0300

    Primeiro Commit

---
+++ Aula04/sql/script_002.sql
@@ -0,0 +1,9 @@
+USE torneio;
+
+ALTER TABLE Jogadores
+    DROP FOREIGN KEY fk_equipe;
+
+ALTER TABLE Jogadores
+    ADD CONSTRAINT fk_equipe FOREIGN KEY (nome_equipe) REFERENCES Equipes (nome)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE;
